7991514620132edce79f06b567b9b6c6dbba61c4
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-10-15 17:03:47 -0400

    Added ability to create and show stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,31 @@
 class StoriesController < ApplicationController
+
+  def index
+    @stories = Story.all.order("created_at DESC")
+  end
+
+  def new
+    @story = Story.new
+  end
+
+  def create
+    @story = Story.new(post_params)
+
+    if @story.save
+      redirect_to @story
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+    @story = Story.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:story).permit(:title,:author,:text)
+  end
 end
