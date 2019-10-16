e819afa2248fdb4f5892091396e0f0e7d0d20a7e
Seth Conklin <sethconklin@Seths-MacBook-Pro.local>
2019-10-16 13:37:23 -0400

    Style changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'stories#welcome', as: 'home'
 
-  get 'create' => 'stories#create_story'
+  get 'build' => 'stories#build_story'
 
   resources :stories
 end
